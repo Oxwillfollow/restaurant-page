d0f5459d49b08661ed6408605d645149d7ec37fc
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2025-12-08 12:19:01 +0200

    Add npm scripts and split webpack config to dev and prod

---
+++ webpack.prod.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'production',
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        template: "./src/template.html",
+    })],
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+        },
+    ],
+  },
+};
